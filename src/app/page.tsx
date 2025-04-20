817c7646e06489034ea183c4427bf8ac316baed1
AxelPere <Paxel1001@gmail.com>
2025-04-20 01:40:11 -0700

    readding the logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import InfoBubble from "@/components/infoBubble";
 import UploadVideo from "@/components/uploadVideo";
+import Image from "next/image";
+import GoosianceName from "@/assets/GoosianceName.webp";
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center gap-6 p-6 bg-gradient-to-b from-goose-red-419 to-goose-red-419 min-h-screen font-irish leading-none tracking-wider">
+      <Image
+        src={GoosianceName}
+        alt="Title"
+        className="w-[60%] md:[25%] lg:w-[50%] ml-4 mb-5"
+      />
+
       <InfoBubble
         text="What is Goosiance?"
         height={100}
@@ -18,7 +26,7 @@ const Home = () => {
         textSize={25}
       />
 
-      <UploadVideo />
+      <UploadVideo/>
 
       <InfoBubble
         text="How To Use Goosiance?"
