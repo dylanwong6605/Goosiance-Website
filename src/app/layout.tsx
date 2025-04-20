dcb705289679b6ad1032f7dd854afa5867dd6aac
AxelPere <Paxel1001@gmail.com>
2025-04-20 00:42:16 -0700

    dylan push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./globals.css"; // import global styles
 import { Inter } from "next/font/google"; // import both fonts
 
 const inter = Inter({ subsets: ["latin"] });
